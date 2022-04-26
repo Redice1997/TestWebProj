4eed3597af82fea8d0cd74491b9d10ea807b9c88
Redice1997 <roshin-vladimir@mail.ru>
2022-04-27 01:00:08 +0300

    Добавить API проект

---
+++ testAPIproj/Controllers/CurrenciesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testAPIproj.Models;
+
+namespace testAPIproj.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrenciesController : Controller
+    {
+        private readonly IMemoryCache memoryCache;
+        public CurrenciesController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+        [HttpGet]        
+        public IEnumerable<CurrencyModel> Index(int pg = 1)
+        {
+            if (!memoryCache.TryGetValue("key_currencies", out List<CurrencyModel> currencies))
+                throw new Exception("Ошибка получения данных");
+
+            const int pageSize = 4;
+            if (pg < 1) pg = 1;
+
+            int recsCount = currencies.Count();
+            var pager = new PagerModel(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = currencies.Skip(recSkip).Take(pager.PageSize);
+
+            return data;
+        }
+        [HttpGet("currency/{id}")]
+        public decimal Currency(string Id)
+        {
+            if (!memoryCache.TryGetValue("key_currencies", out List<CurrencyModel> currencies))
+                throw new Exception("Ошибка получения данных");
+            CurrencyModel currency = currencies.SingleOrDefault(c => c.CharCode.ToUpper() == Id.ToUpper());
+
+            return currency == null ? 0m : (currency.Value / currency.Nominal);
+        }
+    }
+}
---
+++ testAPIproj/Models/CurrencyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testAPIproj.Models
+{
+    public class CurrencyModel
+    {
+        public string ID { get; set; }
+        public int NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public decimal Previous { get; set; }
+    }
+}
